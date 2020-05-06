05564b466758c05ed4d85451bc9218f3398b28ac
Drew Thomasson <superdrew100@Gmail.com>
2020-05-05 23:27:13 -0400

    heroku setup

--- db/connection.js
+++ db/connection.js
@@ -5,12 +5,27 @@ const connectionString = process.env.MONGODB_URI || "mongodb://localhost/covid19
 
 
 mongoose.connect(connectionString)
-    .then(() => {
-        console.log('Connected to mongo successfully!')
-    })
-    .catch((err) => {
-        console.log('Failed to connect to mongo')
-        console.log(err)
-    })
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/<db-name>');
+  }
+  mongoose.connection.on('error', err => {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', () => {
+    console.log("Mongoose has connected to MongoDB!");
+  });
+ //   .then(() => {
+ //       console.log('Connected to mongo successfully!')
+ //   })
+ //   .catch((err) => {
+ //       console.log('Failed to connect to mongo')
+//      console.log(err)
+ //   })
 
 module.exports = mongoose
