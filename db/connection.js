a095b45859f108d220bbff04ad8803b7442a834d
Drew Thomasson <superdrew100@Gmail.com>
2020-05-06 02:07:49 -0400

    another commit

--- db/connection.js
+++ db/connection.js
@@ -15,7 +15,7 @@ if (process.env.MONGODB_URI) {
   mongoose.connection.on('error', err => {
     console.error('MongoDB connection error: ' + err);
     process.exit(-1);
-    }
+    } 
   );
   mongoose.connection.once('open', () => {
     console.log("Mongoose has connected to MongoDB!");
